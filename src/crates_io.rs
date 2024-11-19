eee6db25db17b5920e44a0b582e5b2c69bb0e156
zjp <jiping_zhou@foxmail.com>
2024-11-19 16:48:33 +0800

    crates_io: 下载 crates.io index 仓库需要 1 分钟，改用网络请求

--- src/crates_io.rs
+++ src/crates_io.rs
@@ -1,13 +1,11 @@
 use crate::prelude::*;
 use std::sync::LazyLock;
 
-// https://raw.githubusercontent.com/rust-lang/crates.io-index/refs/heads/master/os/-c/os-checker
+fn url(pkg: &str) -> String {
+    const PREFIX: &str =
+        "https://raw.githubusercontent.com/rust-lang/crates.io-index/refs/heads/master";
 
-const REPO: &str = "https://github.com/rust-lang/crates.io-index.git";
-
-/// Returns the pkg file path if exists
-// ref: https://doc.rust-lang.org/cargo/reference/registry-index.html#index-files
-fn search_pkg_file(pkg: &str, dir: &Utf8Path) -> Option<Utf8PathBuf> {
+    // ref: https://doc.rust-lang.org/cargo/reference/registry-index.html#index-files
     let components = match pkg.len() {
         1 => &["1", pkg][..],
         2 => &["2", pkg],
@@ -21,43 +19,27 @@ fn search_pkg_file(pkg: &str, dir: &Utf8Path) -> Option<Utf8PathBuf> {
             &[a, b, pkg]
         }
     };
-    let path = {
-        let mut path = dir.to_owned();
-        path.extend(components);
-        path
-    };
-    path.exists().then_some(path)
-}
 
-static DIR: LazyLock<Utf8PathBuf> = LazyLock::new(|| {
-    let dir = {
-        const DIR: &str = "crates.io-index";
-        let mut dir = crate::repo::local_base_dir().to_owned();
-        dir.push(DIR);
-        dir
-    };
-    duct::cmd!("git", "clone", REPO, &dir, "--depth", "1")
-        .run()
-        .unwrap();
-    dir
-});
+    // e.g. https://raw.githubusercontent.com/rust-lang/crates.io-index/refs/heads/master/os/-c/os-checker
+    let mut buf = String::with_capacity(128);
+    buf.push_str(PREFIX);
 
-fn count(path: &Utf8Path) -> usize {
-    std::fs::read_to_string(path).unwrap().lines().count()
+    for c in components {
+        buf.push('/');
+        buf.push_str(c);
+    }
+
+    buf
 }
 
 pub fn get_release_count(pkg: &str) -> Option<usize> {
-    search_pkg_file(pkg, &DIR).map(|path| )
+    duct::cmd!("wget", url(pkg), "-O", "-")
+        .read()
+        .ok()
+        .map(|text| text.lines().count())
 }
 
 #[test]
-fn test_get_release_count()  {
+fn test_get_release_count() {
     dbg!(get_release_count("os-checker"));
 }
-
-
-#[test]
-fn test_search_pkg_file()  {
-    dbg!(search_pkg_file("os-checker", &DIR).unwrap());
-}
-
