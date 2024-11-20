cc9ba7fe35f83779398b6110dc3a1427c5d0d5ee
zjp <jiping_zhou@foxmail.com>
2024-11-20 14:26:08 +0800

    feat(release_count): define Data with vers field

--- src/crates_io/release_count.rs
+++ src/crates_io/release_count.rs
@@ -1,3 +1,7 @@
+use crate::prelude::*;
+use cargo_metadata::semver::Version;
+use serde::Deserialize;
+
 fn url(pkg: &str) -> String {
     const PREFIX: &str = "https://index.crates.io";
 
@@ -28,22 +32,38 @@ fn url(pkg: &str) -> String {
     buf
 }
 
-/// NOTE: the result might be spurious due to network failure or invalid text;
-///
-/// None means no release found; 0 is an invalid value because there at least one
-/// release if found.
-pub fn get_release_count(pkg: &str) -> Option<usize> {
+#[derive(Debug, Deserialize)]
+pub struct Data {
+    pub vers: Version,
+}
+
+fn parse_data(index_file: &str) -> Result<Vec<Data>> {
+    serde_json::Deserializer::from_str(index_file)
+        .into_iter()
+        .map(|val| Ok(val?))
+        .collect()
+}
+
+/// NOTE: the error may be due to network failure or invalid text
+pub fn get_data(pkg: &str) -> Result<Vec<Data>> {
     let url = url(pkg);
+    info!("wget {url}");
+
     let output = duct::cmd!("wget", &url, "-O", "-")
         .stdout_capture()
         .stderr_null()
-        .run()
-        .ok()?;
+        .run()?;
 
-    let text = std::str::from_utf8(&output.stdout).ok()?.trim();
-    let count = text.lines().count();
+    let text = std::str::from_utf8(&output.stdout)?.trim();
+    parse_data(text)
+}
+
+/// None means no release found; 0 is an invalid value because there at least one
+/// release if found.
+pub fn get_release_count(pkg: &str) -> Option<usize> {
+    let count = get_data(pkg).ok()?.len();
     if count == 0 {
-        error!(pkg, url, text, "count is an invalid value 0")
+        error!(pkg, "count is an invalid value 0");
     }
     Some(count)
 }
