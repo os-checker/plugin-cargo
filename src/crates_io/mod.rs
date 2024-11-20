ae195aa882ac83f0c7a7a835ec242bed8c8eda6e
zjp <jiping_zhou@foxmail.com>
2024-11-20 14:02:45 +0800

    feat(crates_io): define get_last_release_info

--- src/crates_io/mod.rs
+++ src/crates_io/mod.rs
@@ -1,2 +1,5 @@
 mod release_count;
 pub use release_count::get_release_count;
+
+mod release_tarball;
+pub use release_tarball::get_last_release_info;
---
+++ src/crates_io/release_tarball.rs
@@ -0,0 +1,59 @@
+use crate::{prelude::*, repo::local_base_dir};
+use cargo_metadata::semver::Version;
+use std::os::linux::fs::MetadataExt;
+
+#[derive(Debug)]
+pub struct TarballInfo {
+    /// size in bytes
+    pub size: u64,
+    pub modified: Timestamp,
+}
+
+impl TarballInfo {
+    fn new(tarball: &Utf8Path) -> Result<Self> {
+        let meta = std::fs::metadata(tarball)?;
+        let size = meta.st_size();
+        let modified = Timestamp::from_second(meta.st_mtime())?;
+        Ok(Self { size, modified })
+    }
+}
+
+fn download_tarball(pkg: &str, version: &Version) -> Result<Utf8PathBuf> {
+    const TARBALL: &str = "download";
+
+    let url = url(pkg, version);
+    info!("wget {url}");
+
+    let dir = local_base_dir();
+
+    duct::cmd!("wget", url, "-O", TARBALL)
+        .dir(dir)
+        .stdout_null()
+        .stderr_null()
+        .run()?;
+
+    let tarball = dir.join(TARBALL);
+    Ok(tarball)
+}
+
+fn url(pkg: &str, version: &Version) -> String {
+    // wget https://static.crates.io/crates/os-checker/0.4.1/download
+    // for further use: tar xf download && cd os-checker-0.4.1
+    const PREFIX: &str = "https://static.crates.io/crates";
+    format!("{PREFIX}/{pkg}/{version}/download")
+}
+
+pub fn get_last_release_info(pkg: &str, version: &Version) -> Result<TarballInfo> {
+    let tarball = download_tarball(pkg, version)?;
+    Ok(TarballInfo::new(&tarball)?)
+}
+
+#[test]
+fn test_tarball_info() -> Result<()> {
+    // test on Cargo.toml instead of tarball for now
+    // dbg!(TarballInfo::new("Cargo.toml".into())?);
+
+    dbg!(get_last_release_info("os-checker", &Version::new(0, 4, 1))?);
+
+    Ok(())
+}
