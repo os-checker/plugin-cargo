f2d729fd677acc4f0e035c4b2ca29c284c168b69
zjp <jiping_zhou@foxmail.com>
2025-02-28 22:38:55 +0800

    feat(cache): Repo error or .output() error is also cached

--- src/cache/mod.rs
+++ src/cache/mod.rs
@@ -1,5 +1,6 @@
 use crate::Result;
 use os_checker_plugin_cargo::repo::Repo;
+use plugin::prelude::serde_json;
 
 mod types;
 pub use types::{Api, CachedKey, CachedValue};
@@ -7,10 +8,27 @@ pub use types::{Api, CachedKey, CachedValue};
 mod db;
 mod gh;
 
+/// Output json when error happens.
+fn err_json(user: &str, repo: &str, err: &dyn std::error::Error) -> serde_json::Value {
+    let now = os_checker_types::now();
+    serde_json::json!({
+        "user": user,
+        "repo": repo,
+        "timestamp": {
+            "start": now,
+            "end": now
+        },
+        "err": format!("{err:?}")
+    })
+}
+
 /// Generate a new cached repo and its output regarding tests and package information.
 fn gen_cache(user_repo: &str) -> Result<(CachedKey, CachedValue)> {
     let repo = Repo::new(user_repo)?;
-    let output = repo.output()?;
+    let output = match repo.output() {
+        Ok(output) => output,
+        Err(err) => err_json(&repo.user, &repo.repo, err.as_ref()),
+    };
 
     // remove local dir: all local operations must take place before this
     std::fs::remove_dir_all(&repo.dir)?;
@@ -33,7 +51,13 @@ pub fn get_or_gen_cache(user_repo: &str) -> Result<(CachedKey, CachedValue)> {
     let db = db::Db::open()?;
     let (key, mut val) = match db.load_cache(&key)? {
         Some(val) => (key, val),
-        None => gen_cache(user_repo)?,
+        None => match gen_cache(user_repo) {
+            Ok(cache) => cache,
+            Err(err) => {
+                let val = CachedValue::new(err_json(&key.user, &key.repo, err.as_ref()));
+                (key, val)
+            }
+        },
     };
     val.update_timestamp();
     db.store_cache(&key, &val)?;
